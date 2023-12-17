57778ae82c5ec210a8595a84197bdc750806a722
Christoph Bier <christoph.bier@gmx.de>
2023-12-17 09:14:39 +0100

    Improve and extend solution for day 17 part 2

--- Aoc17/Program.cs
+++ Aoc17/Program.cs
@@ -3,7 +3,16 @@ using static LibAoc.LogUtils;
 
 int SolvePart1(IEnumerable<string> line) {
     var map = line.ToArray();
-    var distances = Djikstra(map);
+    var distances = Djikstra(map, GetNeighborsPart1);
+
+    var info = GetOptimalPath(distances, map.Length - 1, map[0].Length - 1);
+
+    return info.Distance;
+}
+
+int SolvePart2(IEnumerable<string> line) {
+    var map = line.ToArray();
+    var distances = Djikstra(map, GetNeighborsPart2);
 
     var info = GetOptimalPath(distances, map.Length - 1, map[0].Length - 1);
 
@@ -12,7 +21,7 @@ int SolvePart1(IEnumerable<string> line) {
 
 Info GetOptimalPath(Info[,,] distances, int row, int col) {
     var best = distances[row, col, 0];
-    for (var s = 1; s < 12; s++) {
+    for (var s = 1; s < 4; s++) {
         if (distances[row, col, s].Distance < best.Distance) {
             best = distances[row, col, s];
         }
@@ -20,27 +29,31 @@ Info GetOptimalPath(Info[,,] distances, int row, int col) {
     return best;
 }
 
-// We use Djikstra, but nodes are positions + last steps used to enter
-Info[,,] Djikstra(string[] map) {
+Info[,,] Djikstra(string[] map, Func<Node, int, int, IEnumerable<Node>> neighbors) {
     var rows = map.Length;
     var cols = map[0].Length;
     var dists = InitializeNodes(rows, cols);
     var queue = new List<(int, Node)>();
 
-    UpdateDistance(dists, queue, new Node(0, 1, LastSteps.R), Cost(map[0][1]), new Node(0, 0, LastSteps.D));
-    UpdateDistance(dists, queue, new Node(1, 0, LastSteps.D), Cost(map[1][0]), new Node(0, 0, LastSteps.R));
+    var initialNodes = neighbors(new Node(0, 0, Direction.R), rows, cols)
+        .Concat(neighbors(new Node(0, 0, Direction.D), rows, cols));
+
+    var start = new Node(0, 0, Direction.R);
+    foreach (var initial in initialNodes) {
+        UpdateDistance(dists, queue, initial, Dist(map, start, initial), start);
+    }
 
     var currentNode = Dequeue(queue);
     while (currentNode.Row >= 0) {
         var info = GetInfo(dists, currentNode);
-        Log($"Visiting {currentNode.Row}, {currentNode.Column} {currentNode.Steps}, Distance {info.Distance}");
+        Log($"Visiting {currentNode.Row}, {currentNode.Column} {currentNode.LastDirection}, Distance {info.Distance}");
 
         if (currentNode.Row == rows - 1 && currentNode.Column == cols - 1) break;
 
         info.Final = true;
 
-        foreach (var neighbor in GetNeighbors(currentNode, rows, cols)) {
-            var newDistance = info.Distance + Cost(map[neighbor.Row][neighbor.Column]);
+        foreach (var neighbor in neighbors(currentNode, rows, cols)) {
+            var newDistance = info.Distance + Dist(map, currentNode, neighbor);
             UpdateDistance(dists, queue, neighbor, newDistance, currentNode);
         }
 
@@ -55,15 +68,26 @@ void UpdateDistance(Info[,,] dists, List<(int, Node)> queue, Node node, int newD
     var info = GetInfo(dists, node);
     if (info.Final) return;
     if (info.Distance > newDistance) {
-        Log($"\tUpdate {node.Row}, {node.Column} {node.Steps}: {info.Distance} > {newDistance}");
+        Log($"\tUpdate {node.Row}, {node.Column} {node.LastDirection}: {info.Distance} > {newDistance}");
         info.Distance = newDistance;
         info.Predecessor = pred;
         Update(queue, node, newDistance);
     }
 }
 
+int Dist(string[] map, Node a, Node b) {
+    var dr = b.Row == a.Row ? 0 : b.Row > a.Row ? 1 : -1;
+    var dc = b.Column == a.Column ? 0 : b.Column > a.Column ? 1 : -1;
+    var iMax = Math.Max(Math.Abs(b.Row - a.Row), Math.Abs(b.Column - a.Column));
+    var dist = 0;
+    for (var i = 1; i <= iMax; i++) {
+        dist += Cost(map[a.Row + i * dr][a.Column + i * dc]);
+    }
+    return dist;
+}
+
 Node Dequeue(List<(int, Node)> queue) {
-    if (!queue.Any()) return new Node(-1, -1, LastSteps.R);
+    if (!queue.Any()) return new Node(-1, -1, Direction.R);
     var (_, best) = queue[0];
     queue.RemoveAt(0);
     return best;
@@ -81,49 +105,43 @@ void Update(List<(int, Node)> queue, Node node, int distance) {
 }
 
 Info GetInfo(Info[,,] dists, Node node) {
-    return dists[node.Row, node.Column, (int)node.Steps];
+    return dists[node.Row, node.Column, (int)node.LastDirection];
 }
 
-IEnumerable<Node> GetNeighbors(Node node, int rows, int cols) {
+IEnumerable<Node> GetNeighborsPart1(Node node, int rows, int cols) =>
+    GetNeighbors(node, rows, cols, 1, 3);
+
+IEnumerable<Node> GetNeighborsPart2(Node node, int rows, int cols) =>
+    GetNeighbors(node, rows, cols, 4, 10);
+
+IEnumerable<Node> GetNeighbors(Node node, int rows, int cols, int minSteps, int maxSteps) {
     var r = node.Row;
     var c = node.Column;
-    var s = node.Steps;
-    var lastDirection = SingleStep(s);
 
-    if (lastDirection == LastSteps.R || lastDirection == LastSteps.L) {
-        if (r + 1 < rows)
-            yield return new Node(r + 1, c, LastSteps.D);
-        if (r - 1 >= 0)
-            yield return new Node(r - 1, c, LastSteps.U);
+    if (node.LastDirection == Direction.R || node.LastDirection == Direction.L) {
+        for (var k = minSteps; k <= maxSteps; k++) {
+            if (r + k < rows)
+                yield return new Node(r + k, c, Direction.D);
+            if (r - k >= 0)
+                yield return new Node(r - k, c, Direction.U);
+        }
     }
 
-    if (lastDirection == LastSteps.U || lastDirection == LastSteps.D) {
-        if (c + 1 < cols)
-            yield return new Node(r, c + 1, LastSteps.R);
-        if (c - 1 >= 0)
-            yield return new Node(r, c - 1, LastSteps.L);
+    if (node.LastDirection == Direction.U || node.LastDirection == Direction.D) {
+        for (var k = minSteps; k <= maxSteps; k++) {
+            if (c + k < cols)
+                yield return new Node(r, c + k, Direction.R);
+            if (c - k >= 0)
+                yield return new Node(r, c - k, Direction.L);
+        }
     }
-
-    if ((s == LastSteps.D || s == LastSteps.DD) && r + 1 < rows)
-            yield return new Node(r + 1, c, OneMore(s));
-
-    if ((s == LastSteps.U || s == LastSteps.UU) && r - 1 >= 0)
-            yield return new Node(r - 1, c, OneMore(s));
-
-    if ((s == LastSteps.R || s == LastSteps.RR) && c + 1 < cols)
-            yield return new Node(r, c + 1, OneMore(s));
-
-    if ((s == LastSteps.L || s == LastSteps.LL) && c - 1 >= 0)
-            yield return new Node(r, c - 1, OneMore(s));
-
-    yield return new Node(1,1, LastSteps.R);
 }
 
 Info[,,] InitializeNodes(int rows, int cols) {
-    var dists = new Info[rows, cols, 12];
+    var dists = new Info[rows, cols, 4];
     for (var i = 0; i < rows; i++)
         for (var j = 0; j < cols; j++)
-            for (var s = 0; s < 12; s++) {
+            for (var s = 0; s < 4; s++) {
                 dists[i, j, s] = new Info();
             }
     return dists;
@@ -133,34 +151,31 @@ int Cost(char mapChar) {
     return (int)(mapChar - '0');
 }
 
-LastSteps SingleStep(LastSteps steps) {
-    return (LastSteps)(3 * ((int)steps / 3));
-}
-
-LastSteps OneMore(LastSteps steps) {
-    return (LastSteps)((int)steps + 1);
-}
-
 if (args.Length == 0) {
     EnableLogging = true;
-    TestUtils.TestCase("SingleStep", SingleStep, LastSteps.R, LastSteps.R);
-    TestUtils.TestCase("SingleStep", SingleStep, LastSteps.D, LastSteps.D);
-    TestUtils.TestCase("SingleStep", SingleStep, LastSteps.U, LastSteps.U);
-    TestUtils.TestCase("SingleStep", SingleStep, LastSteps.L, LastSteps.L);
-    TestUtils.TestCase("SingleStep", SingleStep, LastSteps.RRR, LastSteps.R);
-    TestUtils.TestCase("SingleStep", SingleStep, LastSteps.DDD, LastSteps.D);
-    TestUtils.TestCase("SingleStep", SingleStep, LastSteps.UUU, LastSteps.U);
-    TestUtils.TestCase("SingleStep", SingleStep, LastSteps.LLL, LastSteps.L);
+    var testMap = new string[] {
+        "13456",
+        "23456",
+        "34567",
+        "54321",
+    };
+    int D((int, int, int, int) t) => Dist(testMap, new Node(t.Item1, t.Item2, Direction.R),
+            new Node(t.Item3, t.Item4, Direction.R));
+    TestUtils.TestCase("Dist", D, (0, 0, 0, 1), 3);
+    TestUtils.TestCase("Dist", D, (0, 0, 1, 0), 2);
+    TestUtils.TestCase("Dist", D, (0, 0, 3, 0), 10);
+    TestUtils.TestCase("Dist", D, (0, 0, 0, 3), 12);
 } else {
     Utils.AocMain(args, SolvePart1);
+    Utils.AocMain(args, SolvePart2);
 }
 
 class Info {
     public Info() { }
     public int Distance { get; set; } = int.MaxValue;
-    public Node Predecessor { get; set; } = new Node(-1, -1, LastSteps.R);
+    public Node Predecessor { get; set; } = new Node(-1, -1, Direction.R);
     public bool Final { get; set; } = false;
 }
 
-record struct Node(int Row, int Column, LastSteps Steps);
-enum LastSteps { R, RR, RRR, D, DD, DDD, L, LL, LLL, U, UU, UUU };
+record struct Node(int Row, int Column, Direction LastDirection);
+enum Direction { R, L, D, U };
