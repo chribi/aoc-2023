d66103ded6068745d115dd031939bb15d5b3246b
Christoph Bier <christoph.bier@gmx.de>
2023-12-27 15:54:09 +0100

    Clean up day 21 part 1 solution

--- Aoc21/Program.cs
+++ Aoc21/Program.cs
@@ -5,19 +5,36 @@ int SolvePart1(IEnumerable<string> line) {
     var map = line.ToArray();
     var width = map[0].Length;
     var height = map.Length;
+    var startPos = (-1, -1);
+    for (var row = 0; row < height; row++) {
+        for (var col = 0; col < width; col++) {
+            if (map[row][col] == 'S') {
+                startPos = (row, col);
+                break;
+            }
+        }
+        if (startPos != (-1, -1)) break;
+    }
+
+    return CountReachable(map, 64, startPos);
+}
+
+int CountReachable(string[] map, int totalSteps, (int, int) startPos) {
+    var width = map[0].Length;
+    var height = map.Length;
     var steps = new int[height, width];
     for (var row = 0; row < height; row++) {
         for (var col = 0; col < width; col++) {
-            steps[row, col] = map[row][col] == 'S' ? 0 : -1;
+            steps[row, col] = (row, col) == startPos ? 0 : -1;
+            if (map[row][col] == '#')
+                steps[row, col] = -2;
         }
     }
-
-    var totalSteps = 64;
 
     for (var s = 0; s < totalSteps; s++) {
         for (var row = 0; row < height; row++) {
             for (var col = 0; col < width; col++) {
-                if (map[row][col] == '#') continue;
+                if (steps[row, col] != -1) continue;
                 if ( (row > 0 && steps[row - 1, col] == s)
                         || (row < height - 1 && steps[row + 1, col] == s)
                         || (col > 0 && steps[row, col - 1] == s)
@@ -29,11 +46,11 @@ int SolvePart1(IEnumerable<string> line) {
         }
     }
 
-
     var reachable = 0;
+    var parity = totalSteps % 2;
     for (var row = 0; row < height; row++) {
         for (var col = 0; col < width; col++) {
-            if (steps[row, col] == totalSteps)
+            if (steps[row, col] >= 0 && steps[row, col] % 2 == parity)
                 reachable++;
         }
     }
